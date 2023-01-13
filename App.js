8a840f3ac8513ef11066446ea7a7a41f2d539d31
Jake Alexander <jaketalexander@gmail.com>
2023-01-13 15:50:26 -0800

    Playground

--- App.js
+++ App.js
@@ -1,9 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TouchableOpacity, Image, SafeAreaView } from 'react-native';
+import React, { useState } from 'react';
+import FriendsList from './Friends';
+import menuIcon from './assets/menu.png';
 
 export default function App() {
+
+  const [isFriendsListVisible, setIsFriendsListVisible] = useState(false);
+
+    const navigateToFriendsList = () => {
+        setIsFriendsListVisible(!isFriendsListVisible);
+    }
+
   return (
     <View style={styles.container}>
+      <View style={styles.menuIconContainer}>
+          <TouchableOpacity onPress={navigateToFriendsList}>
+              <Image source={menuIcon} style={styles.menuIcon} />
+          </TouchableOpacity>
+      </View>
+      {isFriendsListVisible && <FriendsList />}
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
@@ -16,5 +32,14 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  menuIconContainer: {
+    position: 'absolute',
+    top: 60,
+    left: 20,
+  },
+  menuIcon: {
+    width: 30,
+    height: 30,
   },
 });
