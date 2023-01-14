2d7548ebff10a85542aa1344a730c003acb8a9a5
Chris Wong <wongcpie@gmail.com>
2023-01-13 17:13:13 -1000

    setup

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
+import React, {useState} from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [modalVisible, setModalVisible] = useState(false);
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
     </View>
   );
 }
