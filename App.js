8c724a045515ce5771c42d28c666580ce1c44b37
Jake Alexander <jaketalexander@gmail.com>
2023-01-14 10:09:44 -0800

    Added Basic Navigation

--- App.js
+++ App.js
@@ -1,25 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from 'react';
 import LandingPage from './LandingPage'
+import Register1 from './Register1'
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-
   return (
-    <SafeAreaView style={styles.container}>
-      <View>
-        <LandingPage />
-        <StatusBar style="auto" />
-      </View>
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Landing">
+
+        <Stack.Screen
+          name="Landing"
+          component={LandingPage}
+          options={{title: 'Riff', headerShown: false}}
+        />
+
+        <Stack.Screen
+          name="Register1"
+          component={Register1}
+          options={{title: 'Register', headerShown: false}}
+        />
+
+        <Stack.Screen 
+          name="DM"
+          component={DM}
+          options={{title: 'DM', headerShown: false}}
+        />
+
+      </Stack.Navigator>
+      <StatusBar style="auto" />
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#36393e',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
