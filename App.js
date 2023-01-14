636915fd515551a9a585e791b6036d2f16aebfa7
Jake Alexander <jaketalexander@gmail.com>
2023-01-13 17:24:33 -0800

    Landing Page Basic Layout

--- App.js
+++ App.js
@@ -1,45 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button, TouchableOpacity, Image, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 import React, { useState } from 'react';
-import FriendsList from './Friends';
-import menuIcon from './assets/menu.png';
+import LandingPage from './LandingPage'
 
 export default function App() {
 
-  const [isFriendsListVisible, setIsFriendsListVisible] = useState(false);
-
-    const navigateToFriendsList = () => {
-        setIsFriendsListVisible(!isFriendsListVisible);
-    }
-
   return (
-    <View style={styles.container}>
-      <View style={styles.menuIconContainer}>
-          <TouchableOpacity onPress={navigateToFriendsList}>
-              <Image source={menuIcon} style={styles.menuIcon} />
-          </TouchableOpacity>
+    <SafeAreaView style={styles.container}>
+      <View>
+        <LandingPage />
+        <StatusBar style="auto" />
       </View>
-      {isFriendsListVisible && <FriendsList />}
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#36393e',
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  menuIconContainer: {
-    position: 'absolute',
-    top: 60,
-    left: 20,
-  },
-  menuIcon: {
-    width: 30,
-    height: 30,
   },
 });
