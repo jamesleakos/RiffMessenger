833d4317201a380c67eeab0a8534bcfd70b57371
James Leakos <jamesleakos@gmail.com>
2023-01-14 16:54:46 -0700

    some working stuff

---
+++ navigation/userStack.js
@@ -0,0 +1,17 @@
+// for logged - in usersimport React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import HomeScreen from '../screens/Home.js';
+
+const Stack = createStackNavigator();
+
+export default function UserStack() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
---
+++ screens/Landing.js
@@ -0,0 +1,89 @@
+import React from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, Dimensions, Image } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import bee from '../assets/bee.png';
+
+var width = Dimensions.get('window').width;
+
+
+export default function Landing({ navigation }) {
+  return (
+    // <View style={styles.container}>
+    //   <Text>Home screen!</Text>
+    //   <StatusBar style="auto" />
+    // </View>
+    <View style={styles.container}>
+      <View style={styles.topContainer}>
+        <Text style={styles.name}>RIFF</Text>
+        <View style={styles.beeContainer}>
+            <Image source={bee} style={styles.beeImg} />
+        </View>
+      </View>
+      <View style={styles.bottomContainer}>
+        <Text style={styles.welcome}>Welcome to Riff</Text>
+        <Text style={styles.tagline}>Join people who use Riff to talk with communities and friends.</Text>
+          <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Sign Up')}>
+              <Text style={styles.buttonText}>Register</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Sign In')}>
+              <Text style={styles.buttonText}>Login</Text>
+          </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#36393e',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  topContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  bottomContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    padding: 30,
+  },
+  name: {
+      fontSize: 48,
+      color: '#fff',
+  },
+  button: {
+    backgroundColor: '#7289da',
+    padding: 12,
+    borderRadius: 8,
+    marginVertical: 10,
+    width: width * .8,
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: '#fff',
+    textAlign: 'center',
+  },
+  beeContainer: {
+    position: 'absolute',
+    top: 60,
+    left: 20,
+  },
+  beeImg: {
+    width: 60,
+    height: 60,
+  },
+  welcome: {
+    fontSize: 20,
+    color: 'white',
+    padding: 10,
+  },
+  tagline: {
+    color: '#7F7F7F',
+    topPadding: 10,
+    width: width * .5
+  },
+});
