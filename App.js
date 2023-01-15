c6f2668442a3f17ead5ab4a30f39bf2f63301d04
sindwarf <sindorfsolutions@gmail.com>
2023-01-14 16:06:46 -0800

    fixed app

--- App.js
+++ App.js
@@ -46,7 +46,7 @@ export default function App() {
         />
 
       </Stack.Navigator>
-      <StatusBar  barStyle="dark-content" translucent={true} />
+      <StatusBar style="auto" />
 
     </NavigationContainer>
   );
