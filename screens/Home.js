977c3cdf8e26d26223ec3d2649c247bcb57d2808
James Leakos <jamesleakos@gmail.com>
2023-01-14 18:15:41 -0700

    some more auth stuff - mainly just some comments indicating how to protect an API route

--- screens/Home.js
+++ screens/Home.js
@@ -10,6 +10,35 @@ const auth = getAuth();
 export default function HomeScreen() {
   const { user } = useAuthentication();
 
+  // we can get private info like this
+  // axios.get('/private', {
+  //   headers: {
+  //     Authorization: user.token,
+  //   },
+  // })
+
+  // and then on the server side ... 
+
+  // const admin = require('firebase-admin');
+
+  // // Initialize Firebase Admin SDK
+  // admin.initializeApp();
+
+  // // Connect to your other database
+  // const otherDb = require('other-db-library');
+
+  // app.get('/private', async (req, res) => {
+  //   // Verify the user's JWT token
+  //   try {
+  //     const decodedToken = await admin.auth().verifyIdToken(req.headers.authorization);
+  //     // Use the uid from the decoded token to retrieve the user's private data from other db
+  //     const privateData = await otherDb.find({ userId: decodedToken.uid });
+  //     res.send(privateData);
+  //   } catch (error) {
+  //     res.status(401).send('Unauthorized');
+  //   }
+  // });
+
   return (
     <View style={styles.container}>
       <Text>Welcome {user?.email}!</Text>
